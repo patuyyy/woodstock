5901d206f66d841b96e36e0f63062e61ccc9cb87
Ryan <ryansaftje123@gmail.com>
2024-12-13 17:29:14 +0700

    Finishing Prepare to deploy

--- woodstock-fe/src/pages/LandingPage.jsx
+++ woodstock-fe/src/pages/LandingPage.jsx
@@ -1,171 +1,208 @@
-import React from "react";
-import { motion } from "framer-motion";
-import LogoRaw from "../assets/LogoRaw";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import Footer from "../components/Footer";
+import LogoRaw from "../assets/LogoRaw";
+import Navbar from "../components/Navbar";
 
-const LandingPage = () => {
-  const fadeInUp = {
-    hidden: { opacity: 0, y: 40 },
-    visible: { opacity: 1, y: 0, transition: { duration: 1 } },
+const LandingMarketplace = () => {
+  const userInfo = JSON.parse(localStorage.getItem("userInfo")) || {
+    username: "Guest",
   };
 
-  const staggerContainer = {
-    hidden: { opacity: 1 },
-    visible: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.2,
-      },
-    },
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchQuery, setSearchQuery] = useState(""); // State for search query
+  const [loading, setLoading] = useState(true); // State for loading
+  
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/market/`);
+        const data = await response.json();
+
+        if (data.success) {
+          setLoading(false);
+          const productData = data.data.map((item) => ({
+            id: item.id,
+            name: item.name,
+            photo: item.photo,
+            price: item.price,
+            category: item.categories,
+          }));
+          setProducts(productData);
+
+          // Extract unique categories
+          const uniqueCategories = [
+            "All",
+            ...new Set(productData.map((product) => product.category)),
+          ];
+          setCategories(uniqueCategories);
+        } else {
+          console.error("Error fetching products:", data.message);
+        }
+      } catch (error) {
+        console.error("An error occurred:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  // Filter products based on selected category and search query
+  const filteredProducts =
+    selectedCategory === "All"
+      ? products.filter((product) =>
+          product.name.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+      : products
+          .filter((product) => product.category === selectedCategory)
+          .filter((product) =>
+            product.name.toLowerCase().includes(searchQuery.toLowerCase())
+          );
+
+  const scrollToSection = (sectionId) => {
+    const element = document.getElementById(sectionId);
+    const navbarHeight = 80;
+    if (element) {
+      window.scrollTo({
+        top: element.offsetTop - navbarHeight,
+        behavior: "smooth",
+      });
+    }
   };
 
-  const fadeInLeft = {
-    hidden: { opacity: 0, x: -50 },
-    visible: { opacity: 1, x: 0, transition: { duration: 0.2 } },
+
+  const handleAddToCart = (product, event) => {
+    event.preventDefault();  // Prevent the link from being followed
+    event.stopPropagation(); // Stop the click event from propagating to the parent Link
+  
+    // Add the product to the cart in localStorage
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+    cart.push(product);
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    window.location.reload();
   };
-
-  const features = [
-    {
-      title: "Eco-Friendly Design",
-      description:
-        "Promote sustainability with our green metrics. Our designs focus on using environmentally friendly materials and processes, ensuring that every project contributes to reducing carbon footprints.",
-    },
-    {
-      title: "Community Driven",
-      description:
-        "Collaborate with nature enthusiasts worldwide. Join a supportive community where ideas and knowledge are shared for advancing environmental conservation and reforestation efforts.",
-    },
-    {
-      title: "Innovative Features",
-      description:
-        "Explore tools to promote environmental greening. Utilize advanced tools designed to make environmental initiatives more efficient and impactful, fostering long-term sustainability.",
-    },
-  ];
-
 
   return (
-    <div className="bg-white dark:bg-black">
-      {/* Section 1 */}
-      <motion.section
-        className="bg-white dark:bg-black1 transition-all ease-in-out duration-500"
-        initial="hidden"
-        animate="visible"
-        variants={staggerContainer}
-      >
-        <div className="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
-          <motion.div
-            className="mr-auto place-self-center lg:col-span-7"
-            variants={fadeInUp}
-          >
-            <h1 className="max-w-2xl mb-4 text-xl font-title leading-none tracking-tight md:text-2xl xl:text-3xl dark:text-white">
-              Welcome to
-            </h1>
-            <h1>
-              <motion.span
-                className="text-lightGreen bg-black4 dark:bg-black1 px-3 mr-1 dark:mr-0 dark:px-0 rounded-xl dark:text-lightOrange text-9xl font-extrabold font-title text-outline inline-block transition-all ease-in-out duration-1000"
-                variants={fadeInLeft}
-                initial="hidden"
-                animate="visible"
-              >
-                WOOD
-              </motion.span>
-
-              <motion.span
-                className="text-lightOrange dark:text-lightGreen text-9xl font-extrabold font-title text-outline transition-all ease-in-out duration-500"
-                variants={fadeInUp}
-              >
-                STOCK
-              </motion.span>
-            </h1>
-            <p className="max-w-2xl mb-6 mt-5 text-black lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-              WoodStock is a green-metric based web application that accommodates anyone, most importantly nature lovers, to promote environmental greening movement through some web features.
+    <div>
+      <Navbar/>
+      <section className="bg-white dark:bg-black1 transition-all ease-in-out duration-500 pt-20">
+        <header className="bg-white dark:bg-black1 p-5 transition-all ease-in-out duration-500">
+          <h1 className="text-4xl font-title text-start text-black2 dark:text-white mb-2 ease-in-out transition-all duration-500">
+            Welcome {userInfo.username}, to{" "}
+            <span className="text-lightGreen bg-black4 dark:bg-black1 px-1 dark:px-0 rounded-xl dark:text-lightOrange font-title text-outline inline-block ease-in-out transition-all duration-500">
+              Wood
+            </span>
+            <span className="text-darkOrange dark:text-lightGreen ease-in-out transition-all duration-500">
+              Stock
+            </span>
+          </h1>
+          <p className="text-xl font-title text-start text-black3 dark:text-lightGreen">
+            How can we help you today?
+          </p>
+        </header>
+        <div className="grid max-w-screen-xl mx-auto px-4 items-center justify-center text-center pb-20 lg:flex lg:flex-row lg:gap-8">
+          <div className="lg:col-span-2 flex flex-col items-center justify-center">
+            <p className="max-w-2xl mb-4 text-2xl text-black font-title dark:text-gray-400">
+              Browse your trees now
             </p>
-            <Link
-              to="/register"
+            <h1
+              onClick={() => scrollToSection("our-products")}
               className="bg-lightOrange px-4 py-2 text-2xl hover:scale-105 transition-all duration-300 hover:text-lightGreen rounded-xl inline-block font-title text-white"
             >
-              Get Started
-            </Link>
-          </motion.div>
-          <motion.div
-            className="hidden lg:mt-0 lg:col-span-5 lg:flex items-center justify-center"
-            variants={fadeInUp}
-          >
+              Browse trees
+            </h1>
+          </div>
+          <div className="lg:col-span-5 flex items-center justify-center">
             <LogoRaw />
-          </motion.div>
+          </div>
         </div>
-      </motion.section>
+      </section>
 
-      {/* Section 2 */}
-      <motion.section
-        className="bg-white2 dark:bg-black2 transition-all duration-500 min-h-2.5 py-20"
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true }}
-        variants={staggerContainer}
-      >
-        <div className="grid max-w-screen-xl px-4 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:grid-cols-12">
-          <motion.div
-            className="items-center lg:col-span-full justify-center"
-            variants={fadeInUp}
-          >
-            <div className="mr-auto place-self-center items-center justify-center lg:col-span-7">
-              <h1 className="max-w-2xl mb-8 text-3xl bg-black dark:bg-black2 inline-block px-4 py-2 dark:px-0 rounded-xl text-white font-title leading-none tracking-tight md:text-2xl xl:text-4xl transition-all duration-500 text-center">
-                Our Goals
-              </h1>
+      {loading ? (
+        <div className="flex justify-center items-center h-screen bg-white2 dark:bg-black2">
+          <div className="flex flex-col justify-center items-center text-center">
+            <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-lightOrange border-solid"></div>
+            <p className="mt-4 text-xl text-lightOrange font-bold">
+              Loading Products...
+            </p>
+          </div>
+        </div>
+      ) : (
+      <div className="flex flex-col min-h-screen bg-darkwood text-white1">
+        <div className="flex flex-1">
+          <aside className="bg-white2 dark:bg-black2 ease-in-out transition-all duration-500 lg:w-1/5 sm:w-full w-1/3 p-4">
+            <h2 className="font-bold text-leafGreen text-sm sm:text-xl mb-4">Categories</h2>
+            <ul>
+              {categories.map((category) => (
+                <li
+                  key={category}
+                  onClick={() => setSelectedCategory(category)}
+                  className={`cursor-pointer text-lg py-2 ${
+                    selectedCategory === category
+                      ? "font-bold text-lightOrange"
+                      : "text-black dark:text-gray-300"
+                  } hover:text-lightGreen transition-all duration-300 text-sm sm:text-xl`}
+                >
+                  {category}
+                </li>
+              ))}
+            </ul>
+          </aside>
+
+          <main className="flex-grow bg-white1 dark:bg-black4 p-6 ease-in-out transition-all duration-500">
+            {/* Search Bar */}
+            <div className="mb-6">
+              <input
+                type="text"
+                placeholder="Search for a tree..."
+                className="w-full p-3 rounded-md bg-white dark:bg-black3 text-black dark:text-white border border-gray-300 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-lightGreen"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+              />
             </div>
-
-            <div className="flex justify-center items-center">
-              <motion.div
-                className="w-full max-w-5xl grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-center"
-                variants={staggerContainer}
-              >
-                {[" Inspire greater public involvement in reforestation initiatives.", "Provide a transparent platform for landowners and investors to collaborate on environmental projects.", " Empower individuals with the tools and knowledge to track tree growth and environmental impact."].map((text, idx) => (
-                  <motion.div
-                    key={idx}
-                    className="flex items-center shadow-lg hover:scale-110 justify-center p-10 bg-white rounded-xl dark:bg-black4 transition-all duration-500"
-                    variants={fadeInUp}
-                  >
-                    <p className="text-start text-lg font-title hover:scale-105 font-medium text-gray-700 dark:text-gray-300 transition-all duration-500">
-                      {text}
+            
+            <div
+              id="our-products"
+              className="text-2xl text-black dark:text-white font-title mb-5"
+            >
+              Our Products
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
+              {filteredProducts.map((product) => (
+                <Link
+                to='/login'
+                  key={product.id}
+                  className="bg-white2 hover:scale-105 dark:bg-black3 rounded-lg overflow-hidden shadow-md hover:shadow-lg duration-500 ease-in-out border border-gray-200 dark:border-gray-700 p-4 transition-all"
+                >
+                  <div
+                    className="h-40 w-full bg-center bg-cover rounded-xl"
+                    style={{ backgroundImage: `url(${product.photo})` }}
+                  ></div>
+                  <div className="p-4">
+                    <h3 className="text-xl font-title text-gray-900 dark:text-white mb-2">
+                      {product.name}
+                    </h3>
+                    <p className="text-gray-600 font-title text-lg dark:text-gray-300 mb-4">
+                      Rp {product.price ? product.price.toFixed(2) : "N/A"}
                     </p>
-                  </motion.div>
-                ))}
-              </motion.div>
+                    <Link to='/login' className="mt-7 px-4 py-2 bg-lightOrange  text-sm font-medium rounded-md hover:bg-darkOrange transition-all duration-500"
+                    >
+                      Login to see details
+                    </Link>
+                  </div>
+                </Link>
+              ))}
             </div>
-          </motion.div>
+          </main>
         </div>
-      </motion.section>
-
-      {/* Section 3 */}
-      <motion.section
-        className="bg-white dark:bg-black1 py-20 items-start justify-start transition-all duration-500"
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true }}
-        variants={staggerContainer}
-      >
-        <div className="max-w-screen-md px-4 mx-auto text-left items-start justify-start transition-all duration-500">
-          <h2 className="text-4xl font-title text-black dark:text-white mb-8 transition-all duration-500">Why Choose Us?</h2>
-          <motion.div className="space-y-8" variants={staggerContainer}>
-            {features.map((feature, idx) => (
-              <motion.div
-                key={idx}
-                className="p-6 bg-white dark:bg-black4 rounded-xl shadow-md transition-all duration-500"
-                variants={fadeInUp}
-              >
-                <h3 className="text-xl dark:text-white font-bold mb-4 transition-all duration-500">{feature.title}</h3>
-                <p className="text-gray-600 dark:text-white mb-4 transition-all duration-500">{feature.description}</p>
-              </motion.div>
-            ))}
-          </motion.div>
-        </div>
-      </motion.section>
-
-      <Footer />
+      </div>
+      )}
     </div>
   );
 };
 
-export default LandingPage;
+export default LandingMarketplace;
